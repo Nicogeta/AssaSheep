c3125fbccf3bf209c271e4b3d83041df245b39d0
Nicogeta <nicogeta@sfr.fr>
2011-08-22 14:20:07 +0200

    0.3 :)

--- src/com/Nicogeta/AssaSheep/AssaSheepEntityListener.java
+++ src/com/Nicogeta/AssaSheep/AssaSheepEntityListener.java
@@ -33,18 +33,18 @@ public class AssaSheepEntityListener extends EntityListener{
 				Entity e = event2.getDamager();
 				if (e instanceof Player) {
 					Player player = (Player)e;
-					//if(!isInArena(player)) {
-					int damage = event2.getDamage();
-					LivingEntity mob = (LivingEntity)event.getEntity();
-					int health = mob.getHealth();
-					int life = health - damage;
-					if(life <= 0) {
-						player.getWorld().strikeLightning(player.getLocation());
-						player.getServer().broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + player.getName() + " IS AN ASSASSIN !!!");
-					} else {
-						serv.broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + " Save the sheep, save the player !");
+					if(!isInArena(player)) {
+						int damage = event2.getDamage();
+						LivingEntity mob = (LivingEntity)event.getEntity();
+						int health = mob.getHealth();
+						int life = health - damage;
+						if(life <= 0) {
+							player.getWorld().strikeLightning(player.getLocation());
+							player.getServer().broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + player.getName() + " IS AN ASSASSIN !!!");
+						} else {
+							serv.broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + " Save the sheep, save the player !");
+						}
 					}
-					//}
 				} 
 			} else if (theCause == DamageCause.PROJECTILE){
 				EntityDamageByEntityEvent event2 = (EntityDamageByEntityEvent)event;
@@ -54,48 +54,66 @@ public class AssaSheepEntityListener extends EntityListener{
 					if (proj instanceof Arrow) {
 						if (proj.getShooter() instanceof Player) {
 							Player player = (Player)proj.getShooter();
-							//if(!isInArena(player)) {
+							if(!isInArena(player)) {
+								int damage = event2.getDamage();
+								LivingEntity mob = (LivingEntity)event.getEntity();
+								int health = mob.getHealth();
+								int life = health - damage;
+								if(life <= 0) {
+									player.getWorld().strikeLightning(player.getLocation());
+									player.getServer().broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + player.getName() + " IS AN ASSASSIN !!!");
+								} else {
+									serv.broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + " Save the sheep, save the player !");
+								}
+							}
+						}
+					} else if (proj instanceof Egg || proj instanceof Snowball) {
+						Player player = (Player)proj.getShooter();
+						if(!isInArena(player)) {
 							int damage = event2.getDamage();
 							LivingEntity mob = (LivingEntity)event.getEntity();
 							int health = mob.getHealth();
 							int life = health - damage;
-							if(life <= 0) {
-								player.getWorld().strikeLightning(player.getLocation());
-								player.getServer().broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + player.getName() + " IS AN ASSASSIN !!!");
+							if(damage >= 1) {
+								if(life <= 0) {
+									player.getWorld().strikeLightning(player.getLocation());
+									player.getServer().broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + player.getName() + " IS AN ASSASSIN !!!");
+								} else {
+									serv.broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + " Save the sheep, save the player !");
+								}
 							} else {
-								serv.broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + " Save the sheep, save the player !");
+								player.sendMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + "Stop that, bad little boy !");
 							}
-							//}
 						}
-					} else if (proj instanceof Egg || proj instanceof Snowball) {
-						Player player = (Player)proj.getShooter();
-						//if(!isInArena(player)) {
-						int damage = event2.getDamage();
-						LivingEntity mob = (LivingEntity)event.getEntity();
-						int health = mob.getHealth();
-						int life = health - damage;
-						if(damage >= 1) {
-							if(life <= 0) {
-								player.getWorld().strikeLightning(player.getLocation());
-								player.getServer().broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + player.getName() + " IS AN ASSASSIN !!!");
-							} else {
-								serv.broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + " Save the sheep, save the player !");
-							}
-						} else {
-							player.sendMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + "Stop that, bad little boy !");
-						}
-						//}
+					}
+				}
+			}
+		} else if (mobThatJustDied instanceof Player) {
+			EntityDamageEvent ldc = mobThatJustDied.getLastDamageCause();
+			DamageCause theCause = ldc.getCause();
+			Player p = (Player)event.getEntity();
+			if (theCause == DamageCause.ENTITY_EXPLOSION) {
+				EntityDamageByEntityEvent event2 = (EntityDamageByEntityEvent)event;
+				Entity e = event2.getDamager();
+				if(e instanceof Sheep) {
+					int health = p.getHealth();
+					int damage = event2.getDamage();
+					int life = health - damage;
+					if (life <= 0) {
+						serv.broadcastMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + "HAHA Revenge Of The Sheep!");
+					} else {
+						p.sendMessage(ChatColor.RED + "The Big Sheep: " + ChatColor.GREEN + "The Sheep will get you sooner or later " + p.getName());
 					}
 				}
 			}
 		}
 	}
 
-	/*public boolean isInArena(Player player) {
+	public boolean isInArena(Player player) {
 		if(AssaSheep.maHandler != null && AssaSheep.maHandler.isPlayerInArena(player)) {
 			return true;
 		} else {
 			return false;
 		}
-	}*/
+	}
 }
